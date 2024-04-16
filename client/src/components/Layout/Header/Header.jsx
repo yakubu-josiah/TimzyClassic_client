0a922890520a57cc05762646bfd790289bb8e498
Yakubu Josiah <jossysmothest007@gmail.com>
2024-04-16 20:02:14 +0100

    fix: header and footer component UI and structure amend

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from "react-router-dom";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+
+  function checkRoute(route) {
+    if(route === location.pathname) {
+      return true;
+    }
+  }
+
   return (
     <div className="header">
       <div className="header-wrapper">
         <div className="header-items">
 
-          <div className="logo">The Logo</div>
+          <div className="logo"><Link to="/">The Logo</Link></div>
 
           <div className="menu">
             <ul>
-              <li>Shop</li>
-              <li>About</li>
-              <li>Blogs</li>
-              <li>Contact</li>
+              <li><Link className={`${checkRoute("/pages/products") && "header-active"}`} to="/pages/products">Shop</Link></li>
+              <li><Link className={`${checkRoute("/pages/about-us") && "header-active"}`} to="/pages/about-us">About</Link></li>
+              <li><Link className={`${checkRoute("/pages/blogs") && "header-active"}`} to="/pages/blogs">Blogs</Link></li>
+              <li><Link className={`${checkRoute("/pages/contact-us") && "header-active"}`} to="/pages/contact-us">Contact</Link></li>
             </ul>
           </div>
 
           <div className="navIcons">
-            <div>LG</div>
-            <div>SC</div>
-            <div>CT</div>
+            <div><Link to="/my-account"><PersonOutlineOutlinedIcon /></Link></div>
+            <div><Link to="/search/:q"><SearchOutlinedIcon /></Link></div>
+            <div><Link to="/cart"><ShoppingBagOutlinedIcon /></Link></div>
           </div>
           
         </div>
