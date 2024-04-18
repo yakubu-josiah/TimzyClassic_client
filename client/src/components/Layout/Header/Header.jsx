21f7510173e0b9d7689a103b5602d80346b27ed1
Yakubu Josiah <jossysmothest007@gmail.com>
2024-04-18 23:47:32 +0100

    feat: scroll page to top on new route path

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -16,10 +16,14 @@ export default function Header() {
     }
   }
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
       <Announcement />
-      <div className="header">
+      <div className={`header ${!checkRoute("/") && "header-bg"}`}>
         <div className="header-wrapper">
           <div className="header-items">
 
