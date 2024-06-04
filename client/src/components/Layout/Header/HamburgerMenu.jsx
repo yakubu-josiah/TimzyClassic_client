e4364492b78a2a95e14b39543e35005a74aaaae9
Yakubu Josiah <jossysmothest007@gmail.com>
2024-06-04 23:52:18 +0100

    feat: Hamburger Menu created for mobile views

---
+++ client/src/components/Layout/Header/HamburgerMenu.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import './HamburgerMenu.css';
+
+export default function HamburgerMenu() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+
+
+    return (
+        <div className={`HB ${menuOpen ? 'open': ''}`}>
+            <div className="HB_icon" onClick={toggleMenu}>
+                {menuOpen ? <CloseIcon fontSize="large"/> : <ListOutlinedIcon fontSize="large"/>}
+                <p>{menuOpen ? ' ' : 'Menu'}</p>
+            </div>
+            <div className="HB_menu">
+                <div>
+                    <ul>
+                        <li><Link onClick={toggleMenu} to="/pages/products">Shop</Link></li>
+                        <li><Link onClick={toggleMenu} to="/pages/about-us">About</Link></li>
+                        <li><Link onClick={toggleMenu} to="/pages/blogs">Blogs</Link></li>
+                        <li><Link onClick={toggleMenu} to="/pages/contact-us">Contact</Link></li>
+                    </ul>
+                </div>    
+            </div>
+        </div>
+    )
+}
