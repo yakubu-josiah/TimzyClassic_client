10933c7c44de4a552691ff6514f1ebc7ec207dee
Yakubu Josiah <jossysmothest007@gmail.com>
2024-06-05 16:57:49 +0100

    feat: Add initial NotFound page for undeclared routes

---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div><br /><br /> Oops!!! <br /> <br /> This Page is not found OR Still under construction. <br /><br /><br /> </div>
+  )
+}
