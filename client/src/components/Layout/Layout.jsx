ca42d4d5ce84b62431308025794a5dc74f22f66b
Yakubu Josiah <jossysmothest007@gmail.com>
2024-06-05 16:55:17 +0100

    feat: adding the HamburgerMenu and minor updates on styles

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import HamburgerMenu from './Header/HamburgerMenu'
 
 export default function Layout() {
   
   return (
     <div className="layout">
       <Header />
+      <HamburgerMenu />
         <main>
           <Outlet />
         </main>
