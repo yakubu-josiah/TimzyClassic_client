0acc81400268e70a82f4c573f10810e2628f3538
Yakubu Josiah <jossysmothest007@gmail.com>
2024-07-03 23:30:24 +0100

    fix(core&ui): limiting route link tag within button only

--- client/src/pages/Shop/ShopCard.jsx
+++ client/src/pages/Shop/ShopCard.jsx
@@ -6,13 +6,13 @@ export default function ShopCard({data}) {
 
     return (
         <div className="SHC_card">
-            <Link to={`/pages/shop/${data.name}`}>
-                <div className="SHC_img" style={{ backgroundImage: `url(${data.image})` }}>
+            <div className="SHC_img" style={{ backgroundImage: `url(${data.image})` }}>
+                <Link to={`/pages/shop/${data.name}`}>
                     <div className="SHC_title">
                         <p>{data.name}</p>
                     </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
         </div>
     )
 }
