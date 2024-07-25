7c0bf287b629bac909cdb08482bdb0d012c6546e
Yakubu Josiah <jossysmothest007@gmail.com>
2024-07-25 23:54:42 +0100

    refactor(Product-Card): rename ProductSlider to a more descriptive component

---
+++ client/src/components/UI/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./ProductCard.css";
+import { Link } from 'react-router-dom';
+import ProductDetails from './ProductDetails';
+import { routeFormat } from '../../Utils/routeFormat';
+
+export default function ProductCard({data}) {
+
+    return (
+        <div className="PC_wrap">
+            <div className="PC_card" >
+                <Link to={`/shop-collections/${routeFormat(data.name)}/${data.id}`}>
+                    <div className='PC_imgBox'>
+                        <div className="PC_img" style={{ backgroundImage: `url(${data.image})` }}></div>
+                    </div>
+                    <ProductDetails name={data.name} price={data.price} />
+                </Link>
+            </div>
+        </div>
+    )
+}
