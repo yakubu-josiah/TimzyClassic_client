eafbe0e4411eab74e75f5d01a44771b49c887c87
Yakubu Josiah <jossysmothest007@gmail.com>
2024-09-12 16:50:48 +0100

    feat(core): rendering Cart component and sharing state as prop

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,13 +1,16 @@
-import React, {  useEffect } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { useLocation, Link } from "react-router-dom";
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import './Header.css';
 import Announcement from '../Announcement';
+import Cart from '../../UI/Cart';
 
 export default function Header() {
+  const [openCart, setOpenCart] = useState(false)
   const location = useLocation();
+
 
   function checkRoute(route) {
     if(route === location.pathname) {
@@ -30,24 +33,24 @@ export default function Header() {
 
             <div className="HD_menu">
               <ul>
-                <li><Link className={`${checkRoute("shop-collections") && "HD_active"}`} to="shop-collections">Shop</Link></li>
-                <li><Link className={`${checkRoute("about-us") && "HD_active"}`} to="about-us">About</Link></li>
-                <li><Link className={`${checkRoute("blogs") && "HD_active"}`} to="blogs">Blogs</Link></li>
-                <li><Link className={`${checkRoute("contact-us") && "HD_active"}`} to="contact-us">Contact</Link></li>
+                <li><Link className={`${checkRoute("shop-collections") ? "HD_active" : ""}`} to="shop-collections">Shop</Link></li>
+                <li><Link className={`${checkRoute("about-us") ? "HD_active" : ""}`} to="about-us">About</Link></li>
+                <li><Link className={`${checkRoute("blogs") ? "HD_active" : ""}`} to="blogs">Blogs</Link></li>
+                <li><Link className={`${checkRoute("contact-us") ? "HD_active" : ""}`} to="contact-us">Contact</Link></li>
               </ul>
             </div>
 
             <div className="HD_navIcons">
               <div><Link to="/my-account"><PersonOutlineOutlinedIcon /></Link></div>
               <div><Link to="/search/:q"><SearchOutlinedIcon /></Link></div>
-              <div><Link to="/cart"><ShoppingBagOutlinedIcon /></Link></div>
+              <div><span><ShoppingBagOutlinedIcon className={`${openCart ? 'cart' : ''}`} onClick={() => setOpenCart(!openCart)}/></span></div>
             </div>
-
-
             
           </div>
         </div>
-        
+          <div className={`HD_cart ${openCart ? 'open' : ''}`} >
+            <Cart OpenCart={setOpenCart} /> 
+          </div>
       </div>
     </>
   )
